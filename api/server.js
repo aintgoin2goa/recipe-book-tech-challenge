abae9919569bab6cb1273d9ab062f5759e0e0207
Paul Wilson <paul.wilson66@gmail.com>
2018-11-16 14:48:09 +0000

    add CORS

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const listRecipes = require('./routes/list-recipes');
 const viewRecipe = require('./routes/view-recipe');
 
@@ -6,11 +7,13 @@ const viewRecipe = require('./routes/view-recipe');
 const app = express();
 const port = Number(process.env.PORT || 8181);
 
+app.use(cors());
+
 app.get('/recipes', listRecipes);
 app.get('/recipes/:id', viewRecipe);
 
 app.listen(port);
-console.log(`listening on port ${port}`);
+console.log(`API Server listening on port ${port}`);
 
 module.exports = app;
 
