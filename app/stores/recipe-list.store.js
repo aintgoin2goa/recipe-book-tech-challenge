283e892f6161c8640666b85072388ce5ed2674a4
Paul Wilson <paul.wilson66@gmail.com>
2018-11-19 18:55:20 +0000

    don't keep adding the same recipes

--- app/stores/recipe-list.store.js
+++ app/stores/recipe-list.store.js
@@ -2,13 +2,18 @@ import { observable, action } from 'mobx';
 
 import { fetchRecipeList } from '../lib/api';
 
+const notInList = ids => recipe => !(ids.includes(recipe.id));
+
 export default class RecipeList {
     @observable recipes = [];
 
     @action.bound
     fetchRecipes() {
+        const ids = this.recipes.map(r => r.id);
         fetchRecipeList().subscribe(data => {
-            data.recipes.forEach(r => this.recipes.push(r));
-        })
+            data.recipes
+                .filter(notInList(ids))
+                .forEach(r => this.recipes.push(r))
+        });
     }
 }
