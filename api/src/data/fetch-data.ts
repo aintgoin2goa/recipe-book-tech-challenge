b57ab3c14c6ad3d1ccf3c5659eb10f77d1035053
Paul Wilson <paul.wilson@ktech.com>
2020-05-20 19:16:45 +0100

    fetch data from dynamo

--- api/src/data/fetch-data.ts
+++ api/src/data/fetch-data.ts
@@ -1,32 +1,33 @@
 import { RecipeData } from "./types";
+import AWS from "aws-sdk";
+
+const dynamo = new AWS.DynamoDB({
+  endpoint: process.env.DYNAMO_ENDPOINT,
+  region: process.env.AWS_REGION,
+});
+
+const client = new AWS.DynamoDB.DocumentClient({
+  service: dynamo,
+});
+
+const TABLE_NAME = "recipes";
 
 type DataItem = {
   recipes: RecipeData[];
 };
 
-type Data = {
-  [key: string]: DataItem;
-};
-
-const data: Data = {
-  recipes: { recipes: [] },
-  oneRecipe: { recipes: [] },
-  noRecipes: { recipes: [] },
-};
-
-/**
- * A cheating way of enabling different sorts of data to test the UI responds appropiately
- */
-const fetchData = () => {
-  const name = process.env.DATASOURCE || "recipes";
-  return data[name];
-};
-
 export const fetchAllRecipes = async () => {
-  return fetchData();
+  const response = await client.scan({ TableName: TABLE_NAME }).promise();
+  return response.Items;
 };
 
 export const fetchRecipeById = async (id: string) => {
-  const recipes = await fetchAllRecipes();
-  return recipes.recipes.find((r) => r.id === id);
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id,
+    },
+  };
+  const response = await client.get(params).promise();
+  return response.Item;
 };
